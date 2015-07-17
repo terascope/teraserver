3de00e89bff3a451161caacadf9bede864ac8e90
Jared Noble <jsnoble1@gmail.com>
2015-07-17 10:18:01 -0700

    code cleanup

--- plugins/teranaut/index.js
+++ plugins/teranaut/index.js
@@ -8,41 +8,41 @@ var logger, models, baucis, config, passport, userModel;
 var api = {
     _config: undefined,
 
-    config: function(pluginConfig) {
+    config: function (pluginConfig) {
         this._config = pluginConfig;
-        logger = pluginConfig.logger;        
+        logger = pluginConfig.logger;
         baucis = pluginConfig.baucis;
-        passport = pluginConfig.passport; 
+        passport = pluginConfig.passport;
         config = pluginConfig.server_config;
 
         var modelConfig = {
             mongoose: pluginConfig.mongodb,
             logger: logger
-        }
+        };
 
         if (config.teranaut && config.teranaut.models) {
-            models = require(config.teranaut.models)(modelConfig);    
+            models = require(config.teranaut.models)(modelConfig);
         }
         else {
-            models = require('./server/models')(modelConfig);    
+            models = require('./server/models')(modelConfig);
         }
-        
+
         if (config.teranaut && config.teranaut.auth && config.teranaut.auth.user_model) {
             userModel = models[config.teranaut.auth.user_model];
         }
         else {
             userModel = models.User;
         }
-        
+
     },
 
-    static: function() {
+    static: function () {
         return __dirname + '/static';
     },
 
-    init: function() {
+    init: function () {
 
-        if (! (config.teranaut && config.teranaut.models)) {
+        if (!(config.teranaut && config.teranaut.models)) {
             // Configure Baucis to know about the application models
             require('./server/api/baucis')(this._config);
         }
@@ -53,12 +53,12 @@ var api = {
         passport.deserializeUser(userModel.deserializeUser());
     },
 
-    pre: function() {
+    pre: function () {
         this._config.app.use(passport.initialize());
         this._config.app.use(passport.session());
     },
 
-    routes: function(deferred) {
+    routes: function (deferred) {
         // Login function to generate an API token
         this._config.app.use('/api/v1/token', login);
         this._config.app.use('/api/v1/login', login);
@@ -68,51 +68,51 @@ var api = {
 
         // THIS needs to be deferred until after all plugins have had a chance to load
         var config = this._config;
-        deferred.push(function() {
+        deferred.push(function () {
             config.app.use('/api/v1', baucis());
-        })
-
-        this._config.app.post('/login', passport.authenticate('local'), function(req, res) {
-            //res.redirect('/');
-            res.send(200, 'login successful');
         });
 
-        this._config.app.get('/logout', function(req, res) {
+        this._config.app.post('/login', passport.authenticate('local'), function (req, res) {
+            //res.redirect('/');
+            res.status(200).send('login successful');
+        });
+
+        this._config.app.get('/logout', function (req, res) {
             req.logout();
             //res.redirect('/');
-            res.send(200, 'logout successful');
+            res.status(200).send('logout successful');
         });
     },
 
-    post: function() {
+    post: function () {
 
     }
-}
+};
 
-var ensureAuthenticated = function(req, res, next) {
+var ensureAuthenticated = function (req, res, next) {
     // We allow creating new accounts without authentication.    
     if (config.teranaut.auth.open_signup) {
         // TODO: THIS URL should depend on the name of the model
-        if (req.url === '/accounts' && req.method === 'POST') return next();    
+        if (req.url === '/accounts' && req.method === 'POST') return next();
     }
-    
+
     // See if the session is authenticated
     if (req.isAuthenticated()) {
         return next();
     }
     // API auth based on tokens
     else if (req.query.token) {
-        userModel.findOne({api_token: req.query.token}, function(err, account) {
+        userModel.findOne({api_token: req.query.token}, function (err, account) {
             if (err) {
                 throw err;
             }
 
             if (account) {
                 req.user = account;
-                
+
                 // If there's redis session storage available we add the login to the session.
-                if (config.teraserver && config.teraserver.redis_sessions) {        
-                    req.logIn(account, function(err) {
+                if (config.teraserver && config.teraserver.redis_sessions) {
+                    req.logIn(account, function (err) {
                         if (err) {
                             return next(err);
                         }
@@ -125,7 +125,7 @@ var ensureAuthenticated = function(req, res, next) {
                 }
             }
             else {
-                return res.json(401, { error: 'Access Denied' });
+                return res.status(401).json({error: 'Access Denied'});
             }
         })
     }
@@ -133,34 +133,34 @@ var ensureAuthenticated = function(req, res, next) {
         // For session based auth
 
         //res.redirect('/login')
-        return res.json(401, { error: 'Access Denied' });
+        return res.status(401).json({error: 'Access Denied'});
     }
-}
+};
 
-var login = function(req, res, next) {
+var login = function (req, res, next) {
 
-    passport.authenticate('local', { session: false }, function(err, user, info) {
+    passport.authenticate('local', {session: false}, function (err, user, info) {
 
         if (err) {
             return next(err);
         }
 
-        if (! user) {
-            return res.json(401, { error: info.message });
+        if (!user) {
+            return res.status(401).json({error: info.message});
         }
 
-        if (config.teranaut.auth && config.teranaut.auth.require_email && ! user.email_validated) {            
-            return res.json(401, { error: 'Account has not been activated' });    
+        if (config.teranaut.auth && config.teranaut.auth.require_email && !user.email_validated) {
+            return res.status(401).json({error: 'Account has not been activated'});
         }
 
-        req.logIn(user, function(err) {
+        req.logIn(user, function (err) {
             if (err) {
                 return next(err);
             }
 
             var shasum = crypto.createHash('sha1');
             var date = Date.now();
-            crypto.randomBytes(128, function(err, buf) {
+            crypto.randomBytes(128, function (err, buf) {
                 if (err) {
                     logger.error("Error generating randomBytes on User save.");
                     return next(err);
@@ -178,6 +178,6 @@ var login = function(req, res, next) {
             });
         });
     })(req, res, next);
-}
+};
 
 module.exports = api;
--- plugins/teranaut/server/models/User.js
+++ plugins/teranaut/server/models/User.js
@@ -2,33 +2,33 @@
 
 var model;
 
-module.exports = function(config) {
+module.exports = function (config) {
     var logger = config.logger;
     var mongoose = config.mongoose;
 
-    if (! model) {
+    if (!model) {
         var passportLocalMongoose = require('passport-local-mongoose');
         var crypto = require('crypto');
 
         var Schema = mongoose.Schema;
 
         var userSchema = new Schema({
-            client_id:      { type: Number, required: true },
-            firstname:      String,
-            lastname:       String,
-            role:           {
-                type:       String,
-                required:   true,
-                enum:       ['admin', 'analyst', 'user', 'domains-user'],
-                default:    'user'
+            client_id: {type: Number, required: true},
+            firstname: String,
+            lastname: String,
+            role: {
+                type: String,
+                required: true,
+                enum: ['admin', 'analyst', 'user', 'domains-user'],
+                default: 'user'
             },
-            username:       { type: String, trim: true, required: true, unique: true },
+            username: {type: String, trim: true, required: true, unique: true},
             /*password:       { type: String, trim: true, required: true },*/
-            email:          { type: String, trim: true, lowercase: true },
-            api_token:      String,
+            email: {type: String, trim: true, lowercase: true},
+            api_token: String,
 
-            created:        { type: Date, default: Date.now }, // Date the network was added
-            updated:        { type: Date, default: Date.now },
+            created: {type: Date, default: Date.now}, // Date the network was added
+            updated: {type: Date, default: Date.now}
         });
 
         userSchema.plugin(passportLocalMongoose);
@@ -39,7 +39,7 @@ module.exports = function(config) {
             if (!this.isModified('hash')) return next();
 
             // Hash is passed in as clear text so we need to hash it again
-            this.setPassword(this.hash, function(err, self) {
+            this.setPassword(this.hash, function (err, self) {
                 if (err) {
                     // TODO: we need a proper logger instance here
                     logger.error("Error setting password.");
@@ -47,7 +47,7 @@ module.exports = function(config) {
                 }
 
                 var shasum = crypto.createHash('sha1');
-                crypto.randomBytes(128, function(err, buf) {
+                crypto.randomBytes(128, function (err, buf) {
                     if (err) {
                         logger.error("Error generating randomBytes on User save.");
                         return next(err);
@@ -62,10 +62,10 @@ module.exports = function(config) {
             })
         });
 
-        userSchema.index({ 'client_id': 1, 'username' : 1 });
+        userSchema.index({'client_id': 1, 'username': 1});
 
         model = mongoose.model('User', userSchema);
     }
 
     return model;
-}
+};
