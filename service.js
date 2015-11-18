347ca19166a399835b7fe532d1ad2ae7a06a2a45
Jared Noble <jsnoble1@gmail.com>
2015-11-18 13:57:08 -0700

    broke up plugins schema from system schema

--- service.js
+++ service.js
@@ -1,9 +1,11 @@
 var worker = require('./lib/worker');
 var config_schema = require('./system_schema').config_schema;
+var plugin_schema = require('./system_schema').plugin_schema;
 
 var foundation = require('terafoundation')({
     name: 'teraserver',
     baucis: true,
     worker: worker,
-    config_schema: config_schema
+    config_schema: config_schema,
+    plugin_schema: plugin_schema
 });
