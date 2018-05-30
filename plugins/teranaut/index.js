29ae6e402497d3fa05eb3f7454e39ef3fd03766e
Jared Noble <jsnoble1@gmail.com>
2018-05-30 08:02:29 -0700

    formatted and updated index code

--- plugins/teranaut/index.js
+++ plugins/teranaut/index.js
@@ -1,49 +1,50 @@
 'use strict';
 
-const teranaut_schema = require('./schema');
+const teranautSchema = require('./schema');
 const Promise = require('bluebird');
 const Strategy = require('passport-local').Strategy;
 const parseError = require('error_parser');
 const _ = require('lodash');
-let logger, context, router, config, passport, teranaut, userStore, teraSearchApi;
+
+let logger;
+let context;
+let router;
+let config;
+let passport;
+let teranaut;
+let userStore;
+let teraSearchApi;
 
 const api = {
     _config: undefined,
-    config_schema: () => {
-        return teranaut_schema;
-    },
+    config_schema: () => teranautSchema,
     config: (pluginConfig) => {
         this._config = pluginConfig;
-        context = pluginConfig.context;
-        logger = pluginConfig.logger;
-        passport = pluginConfig.passport;
-        config = pluginConfig.server_config;
-        teranaut = pluginConfig.server_config.teranaut;
+        ({
+            context, logger, passport, server_config: config, server_config: { teranaut }
+        } = pluginConfig);
         router = pluginConfig.express.Router();
-        teraSearchApi = pluginConfig.search(pluginConfig, 'created')
+        teraSearchApi = pluginConfig.search(pluginConfig, 'created');
     },
-    static: () => __dirname + '/static',
-    init: () => {
-        return Promise.resolve()
-            .then(() => require('./server/store/users')(context))
-            .then(_userStore => {
-                userStore = _userStore;
+    static: () => `${__dirname}/static`,
+    init: () => Promise.resolve()
+        .then(() => require('./server/store/users')(context))
+        .then((_userStore) => {
+            userStore = _userStore;
 
-                passport.use(new Strategy(
-                    function(username, password, done) {
-                        if (!username || !password) return done(null, false);
-                        userStore.authenticateUser(username, password)
-                            .then((user) => {
-                                if (!user) return done(null, false);
-                                return done(null, user);
-                            })
-                            .catch(err => done(err));
-                    }));
+            passport.use(new Strategy(((username, password, done) => {
+                if (!username || !password) return done(null, false);
+                userStore.authenticateUser(username, password)
+                    .then((user) => {
+                        if (!user) return done(null, false);
+                        return done(null, user);
+                    })
+                    .catch(err => done(err));
+            })));
 
-                passport.serializeUser(userStore.serializeUser);
-                passport.deserializeUser(userStore.deserializeUser);
-            })
-    },
+            passport.serializeUser(userStore.serializeUser);
+            passport.deserializeUser(userStore.deserializeUser);
+        }),
     pre: () => {
         this._config.app.use(passport.initialize());
         this._config.app.use(passport.session());
@@ -59,29 +60,29 @@ const api = {
         require('./server/api/user')(router, userStore, logger, teraSearchApi);
 
         if (config.teranaut.ui) {
-            const url_base = this._config.url_base;
-            this._config.app.get(url_base, function(req, res) {
-                res.redirect(url_base + '/_'); // redirecting to a path handled by /* path below
+            const { url_base: urlBase } = this._config;
+            this._config.app.get(urlBase, (req, res) => {
+                res.redirect(`${urlBase}/_`); // redirecting to a path handled by /* path below
             });
-            this._config.app.get(url_base + '/', index);
-            this._config.app.get(url_base + '/*', index);
+            this._config.app.get(`${urlBase}/`, index);
+            this._config.app.get(`${urlBase}/*`, index);
 
             // TODO: this is directly hooking to the alias which is making an assumption
             // about the URL space. This ability probably should be moved into teraserver.
-            this._config.app.get('/pl/' + config.teranaut.ui + '/', index);
-            this._config.app.get('/pl/' + config.teranaut.ui + '/*', index);
+            this._config.app.get(`/pl/${config.teranaut.ui}/`, index);
+            this._config.app.get(`/pl/${config.teranaut.ui}/*`, index);
         }
         // THIS needs to be deferred until after all plugins have had a chance to load
-        const plugin_config = this._config;
+        const pluginConfig = this._config;
 
-        deferred.push(function() {
-            plugin_config.app.use('/api/v1', router);
+        deferred.push(() => {
+            pluginConfig.app.use('/api/v1', router);
         });
 
-        this._config.app.post('/login', passport.authenticate('local'), function(req, res) {
+        this._config.app.post('/login', passport.authenticate('local'), (req, res) => {
             res.status(200).send('login successful');
         });
-        this._config.app.get('/logout', function(req, res) {
+        this._config.app.get('/logout', (req, res) => {
             req.logout();
             res.status(200).send('logout successful');
         });
@@ -90,66 +91,62 @@ const api = {
 };
 
 function index(req, res) {
-    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
-    res.header("Pragma", "no-cache");
-    res.header("X-Frame-Options", "Deny");
-    res.sendfile('index.html', {root: __dirname + '/static'});
+    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.header('Pragma', 'no-cache');
+    res.header('X-Frame-Options', 'Deny');
+    res.sendfile('index.html', { root: `${__dirname}/static` });
 }
 
 function ensureAuthenticated(req, res, next) {
     // We allow creating new accounts without authentication.
-    const token = req.query.token;
+    const { token } = req.query;
     if (teranaut.auth.open_signup) {
         if (req.url === '/accounts' && req.method === 'POST') return next();
     }
     // See if the session is authenticated
-    if (req.isAuthenticated()) {
-        return next();
-    }
+    if (req.isAuthenticated()) return next();
+
     // API auth based on tokens
     else if (token) {
         userStore.findByToken(token)
             .then((account) => {
                 if (account) {
                     req.user = account;
-                    // If there's elasticsearch session storage available we add the login to the session.
+                    // If there's session storage available we add the login to the session.
                     if (config.teraserver.elasticsearch_sessions) {
-                        req.logIn(account, function(err) {
+                        req.logIn(account, (err) => {
                             if (err) {
                                 return next(err);
                             }
                             return next();
                         });
-                    }
-                    else {
+                    } else {
                         return next();
                     }
-                }
-                else {
-                    return res.status(401).json({error: 'Access Denied'});
+                } else {
+                    return res.status(401).json({ error: 'Access Denied' });
                 }
             })
-            .catch((err) => next(new Error(err)))
-    }
-    else {
+            .catch(err => next(new Error(err)));
+    } else {
         // For session based auth
-        return res.status(401).json({error: 'Access Denied'});
+        return res.status(401).json({ error: 'Access Denied' });
     }
 }
 
 function login(req, res, next) {
-    passport.authenticate('local', { session: false }, function(err, user, info) {
+    passport.authenticate('local', { session: false }, (err, user, info) => {
         if (err) return next(err);
         if (!user) res.status(401).json({ error: _.get(info, 'message', 'no user was found') });
         if (teranaut.auth.require_email && !user.email_validated) {
             return res.status(401).json({ error: 'Account has not been activated' });
         }
 
-        req.logIn(user, function(err) {
-            if (err) return next(err);
+        req.logIn(user, (errObj) => {
+            if (errObj) return next(errObj);
 
             userStore.createApiTokenHash(user)
-                .then((hashedUser) => userStore.updateToken(hashedUser))
+                .then(hashedUser => userStore.updateToken(hashedUser))
                 .then((hashedUser) => {
                     res.json({
                         token: hashedUser.api_token,
@@ -157,9 +154,9 @@ function login(req, res, next) {
                         id: hashedUser.id
                     });
                 })
-                .catch((err) => {
-                    const errMsg = parseError(err);
-                    logger.error(`error while creating new token and updating user, error:${errMsg}`)
+                .catch((lastErr) => {
+                    const errMsg = parseError(lastErr);
+                    logger.error(`error while creating new token and updating user, error:${errMsg}`);
                     res.status(401).json({ error: 'error while creating new token and updating user' });
                 });
         });
