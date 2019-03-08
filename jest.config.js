83042cd52d6434ed00376ee1f2da3f8f7677a1d2
peterdemartini <thepeterdemartini@gmail.com>
2019-03-08 11:00:14 -0700

    use jest not jasmine and update build scripts

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+    rootDir: '.',
+    verbose: true,
+    setupFilesAfterEnv: [
+        'jest-extended'
+    ],
+    testMatch: [
+        '<rootDir>/test/*-spec.js'
+    ],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '<rootDir>/lib/**/*.js',
+        '<rootDir>/plugins/teranaut/*/*.js',
+        '!<rootDir>/plugins/teranaut/node_modules',
+    ],
+    coverageReporters: ['lcov', 'text', 'html'],
+    coverageDirectory: '<rootDir>/coverage'
+};
