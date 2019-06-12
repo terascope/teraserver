508d5e35ed120798f4303912d1c76e6459b527d1
peterdemartini <thepeterdemartini@gmail.com>
2019-06-12 09:26:14 -0700

    Make authentication for /api/v1 configurable

--- plugins/teranaut/index.js
+++ plugins/teranaut/index.js
@@ -10,12 +10,12 @@ const teranautSchema = require('./schema');
 let logger;
 let context;
 let router;
-let config;
 let passport;
 let teranaut;
 let userStore;
 let aclManager;
 let teraSearchApi;
+let useV1Users;
 
 const api = {
     _config: undefined,
@@ -26,9 +26,9 @@ const api = {
             context,
             logger,
             passport,
-            server_config: config,
             server_config: { teranaut }
         } = pluginConfig);
+        useV1Users = _.get(teranaut, 'auth.use_v1_users', true);
         router = pluginConfig.express.Router();
         teraSearchApi = pluginConfig.search(pluginConfig, 'created');
     },
@@ -57,33 +57,25 @@ const api = {
                         return;
                     }
 
-                    aclManager
-                        .authenticate({ username, password })
-                        .then(user => done(null, user))
+                    userStore
+                        .authenticateUser(username, password)
+                        .then((user) => {
+                            if (!user) return done(null, false);
+                            return done(null, user);
+                        })
                         .catch(err => done(err));
                 })
             );
 
-            passport.serializeUser((user, next) => {
-                next(null, user.username);
-            });
-
-            passport.deserializeUser((username, next) => {
-                aclManager
-                    .findUser({ id: username }, false)
-                    .then((user) => {
-                        next(null, user);
-                    })
-                    .catch((err) => {
-                        next(err);
-                    });
-            });
+            passport.serializeUser(userStore.serializeUser);
+            passport.deserializeUser(userStore.deserializeUser);
         }),
     pre: () => {
         this._config.app.use(passport.initialize());
         this._config.app.use(passport.session());
     },
     routes: (deferred) => {
+        logger.info(`Using ${useV1Users ? 'v1' : 'v2'} users for /api/v1`);
         // Login function to generate an API token
         this._config.app.use('/api/v1/token', login);
         this._config.app.use('/api/v1/login', login);
@@ -93,7 +85,7 @@ const api = {
 
         require('./server/api/user')(router, userStore, logger, teraSearchApi);
 
-        if (config.teranaut.ui) {
+        if (teranaut.ui) {
             const { url_base: urlBase } = this._config;
             this._config.app.get(urlBase, (req, res) => {
                 res.redirect(`${urlBase}/_`); // redirecting to a path handled by /* path below
@@ -103,8 +95,8 @@ const api = {
 
             // TODO: this is directly hooking to the alias which is making an assumption
             // about the URL space. This ability probably should be moved into teraserver.
-            this._config.app.get(`/pl/${config.teranaut.ui}/`, index);
-            this._config.app.get(`/pl/${config.teranaut.ui}/*`, index);
+            this._config.app.get(`/pl/${teranaut.ui}/`, index);
+            this._config.app.get(`/pl/${teranaut.ui}/*`, index);
         }
 
         // THIS needs to be deferred until after all plugins have had a chance to load
@@ -149,21 +141,37 @@ function ensureAuthenticated(req, res, next) {
 
     // API auth based on tokens
     if (token) {
-        aclManager
-            .authenticate({ token })
-            .then((account) => {
-                if (account) {
+        if (useV1Users) {
+            userStore
+                .findByToken(token)
+                .then((account) => {
+                    if (account) {
+                        req.user = account;
+                        return next();
+                    }
+                    return res.status(401).json({ error: 'Access Denied' });
+                })
+                .catch((err) => {
+                    const errMsg = parseError(err);
+                    logger.error(errMsg);
+                    return res.status(503).json({ error: errMsg });
+                });
+        } else {
+            aclManager
+                .authenticate({ token })
+                .then((account) => {
                     req.user = account;
                     req.v2User = account;
                     return next();
-                }
-                return res.status(401).json({ error: 'Access Denied' });
-            })
-            .catch((err) => {
-                const errMsg = parseError(err);
-                logger.error(errMsg);
-                return res.status(503).json({ error: errMsg });
-            });
+                })
+                .catch((err) => {
+                    if ([401, 403, 404].includes(err.statusCode)) {
+                        return res.status(401).json({ error: 'Access Denied' });
+                    }
+                    logger.error(err, 'Failure authenticating user');
+                    return res.status(503).json({ error: err.message });
+                });
+        }
     } else {
         // For session based auth
         res.status(401).json({ error: 'Access Denied' });
@@ -191,21 +199,40 @@ function login(req, res, next) {
                 return;
             }
 
-            aclManager
-                .updateToken({ id: user.id }, false)
-                .then((token) => {
-                    res.json({
-                        token,
-                        date: user.updated,
-                        id: user.id
+            if (useV1Users) {
+                userStore
+                    .createApiTokenHash(user)
+                    .then(hashedUser => userStore.updateToken(hashedUser))
+                    .then((hashedUser) => {
+                        res.json({
+                            token: hashedUser.api_token,
+                            date: hashedUser.updated,
+                            id: hashedUser.id
+                        });
+                    })
+                    .catch((lastErr) => {
+                        logger.error(lastErr, 'error while creating new token and updating user');
+                        return res
+                            .status(401)
+                            .json({ error: 'error while creating new token and updating user' });
                     });
-                })
-                .catch((lastErr) => {
-                    logger.error(lastErr, 'error while creating new token and updating user');
-                    return res
-                        .status(401)
-                        .json({ error: 'error while creating new token and updating user' });
-                });
+            } else {
+                aclManager
+                    .updateToken({ id: user.id }, false)
+                    .then((token) => {
+                        res.json({
+                            token,
+                            date: user.updated,
+                            id: user.id
+                        });
+                    })
+                    .catch((lastErr) => {
+                        logger.error(lastErr, 'error while creating new token and updating user');
+                        return res
+                            .status(401)
+                            .json({ error: 'error while creating new token and updating user' });
+                    });
+            }
         });
     })(req, res, next);
 }
